6781b151163dc1921582b8da9a6c158f28567a7b
IbnRasAl-Fakih <rasul.eletai@gmail.com>
2023-05-01 21:39:35 +0600

    all methods have created excepting hash()

--- MyHashTable.java
+++ MyHashTable.java
@@ -12,7 +12,7 @@ public class MyHashTable<K, V> {
 
         @Override
         public String toString() {
-            return '{' + (String)key + " " + (String)value + '}';
+            return  "{" + key + " " + value + "}";
         }
     }
 
@@ -39,4 +39,92 @@ public class MyHashTable<K, V> {
         return size == 0;
     }
 
+    private int hash(K key) {
+        return 1;
+    }
+
+    public void put(K key, V value) {
+        int index = hash(key);
+        HashNode<K, V> head = buckets[index];
+        HashNode<K, V> temp = head;
+        while (temp != null) {
+            if (temp.key == key) {
+                temp.value = value;
+                return;
+            }
+            temp = temp.next;
+        }
+        size++;
+        HashNode<K, V> node = new HashNode<>(key, value);
+        node.next = head;
+        buckets[index] = node;
+    }
+
+    public V get(K key) {
+        int index = hash(key);
+        HashNode<K, V> temp = buckets[index];
+        while (temp != null) {
+            if (temp.key == key) {
+                return temp.value;
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
+
+    public V remove(K key) {
+        int index = hash(key);
+        HashNode<K, V> temp = buckets[index];
+        HashNode<K, V> previousNode = null;
+        while(temp != null) {
+            if (temp.key == key) {
+                if (previousNode == null) {
+                    buckets[index] = temp.next;
+                } else {
+                    previousNode.next = temp.next;
+                }
+                size--;
+                return temp.value;
+            }
+            previousNode = temp;
+            temp = temp.next;
+        }
+        return null;
+    }
+
+    public boolean contains(V value) {
+        for (int index = 0; index < size; index++) {
+            HashNode<K, V> temp = buckets[index];
+            while (temp != null) {
+                if (temp.value == value) {
+                    return true;
+                }
+                temp = temp.next;
+            }
+        }
+        return false;
+    }
+
+    public K getKey(V value) {
+        for (int index = 0; index < size; index++) {
+            HashNode<K, V> temp = buckets[index];
+            while (temp != null) {
+                if (temp.value == value) {
+                    return temp.key;
+                }
+                temp = temp.next;
+            }
+        }
+        return null;
+    }
+
+    public void print() {
+        for (int index = 0; index < size; index++) {
+            HashNode<K, V> temp = buckets[index];
+            while (temp != null) {
+                System.out.println(temp.toString());
+                temp = temp.next;
+            }
+        }
+    }
 }
